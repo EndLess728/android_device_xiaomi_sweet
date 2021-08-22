9d076eb43ee672a313442346392feab5fc58bd56
Mr. EndLess <aryanmk.mk5@gmail.com>
2021-08-22 11:59:51 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-ssos
+    $(LOCAL_PATH)/overlay-lineage
 
 # rro_overlays
 PRODUCT_PACKAGES += \
